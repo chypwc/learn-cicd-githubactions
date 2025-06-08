a0e20174b55ef43bd9aadde19572bfa600a15e03
chypwc <chienyeh5@gmail.com>
2025-06-08 16:49:13 +1000

    added deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./vitest.setup.js"],
   },
-  base: "/learn-cicd-githubactions",
+  base: "/learn-cicd-githubactions/",
 });
